2224fa3c4b33fbd3d9b942a27d1fea55d84602ee
rzoktan <rz.oktan@gmail.com>
2023-06-01 16:09:00 +0700

    update rounded box

--- components/OpenSourceMemo.tsx
+++ components/OpenSourceMemo.tsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 import { RoundedBox } from "./RoundedBox"
 
 export const OpenSourceMemo = () => (
-  <RoundedBox>
-    <b>Would you like to build similar app?</b> Feel free to browse the&nbsp;
-    <Link href="https://github.com/mateuszsokola/eth-faucet" passHref>
-      <MuiLink target="_blank" rel="noopener referrer">
-        source code on Github
-      </MuiLink>
-    </Link>
-  </RoundedBox>
+  // <RoundedBox>
+  //   <b>Would you like to build similar app?</b> Feel free to browse the&nbsp;
+  //   <Link href="https://github.com/mateuszsokola/eth-faucet" passHref>
+  //     <MuiLink target="_blank" rel="noopener referrer">
+  //       source code on Github
+  //     </MuiLink>
+  //   </Link>
+  // </RoundedBox>
 )
